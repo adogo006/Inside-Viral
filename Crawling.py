e1b454016f74e8f52698d1fc1a50526c795a4182
adogo006 <peand006@gmail.com>
2026-03-09 00:11:04 +0900

    feat: 메인 url 입력시 {period}일 내의 게시물에 접속하여 제목과 내용을 {words}리스트에 저장하고 터미널에 출력하는 기능 추가

--- Crawling.py
+++ Crawling.py
@@ -1,3 +1,4 @@
+#!!!!!반드시 vpn 키고 돌릴것!!!!!
 from urllib.request import urlopen, Request
 from urllib.request import HTTPError
 from urllib.request import URLError
@@ -5,37 +6,95 @@ from urllib.request import URLError
 import re
 
 from bs4 import BeautifulSoup
-import json
-# url을 통해 접속하고 각 단어를 리턴하는 함수
-#일단 게시글 안에서는 script 태그 안 속성  'type': 'application/ld+json' 안에 시간 내용 제목 다 들어가 있음
+from datetime import datetime
+import time
+from random import randint
+# html 내부 div 태그 class 속성 view_content_wrap 내부에서 제목 글 시간 모두 크롤링 가능 
+# write_div
+# 본문의 제목 크롤링 함수, 1.기간내의 게시물인지 확인 2. 제목과 게시글 파싱 및 단어리스트 반환
+def contentCrawler(words, gallId, dataNum, now, period):
+    print('크롤링 중...')
+    time.sleep(0.1 * randint(1, 3))
+    url = 'https://gall.dcinside.com/mgallery/board/view/?id={}&no={}&page=1'.format(gallId, dataNum)
 
-def contentCrawler(url):
-    req = Request(url, headers= {'User-Agent': 'Mozila/5.0'})
+    try:
+        req = Request(url, headers= {'User-Agent': 'Mozilla/5.0'})
+        html = urlopen(req)
+    except HTTPError as e:
+        return contentCrawler(words, gallId, int(dataNum)-1, now, period)
+    else:
+        print('url 접속 성공!')
+        pass
+    bs = BeautifulSoup(html, 'html.parser')
+    contentWrap = bs.find('div', {'class': 'view_content_wrap'})
+    upTime = contentWrap.find('span', {'class': 'gall_date'})
+    #만약 삭제된 게시글이라 upTime이 None이 지정되면 다음 게시글로 이동해서 파싱
+    if upTime == None:
+        return contentCrawler(words, gallId, int(dataNum)-1, now, period)
+    
+    upTimeTitle = upTime.attrs['title']
+
+    dt = datetime.strptime(upTimeTitle, "%Y-%m-%d %H:%M:%S")
+    diff = now - dt
+    if diff.days <= period:
+        title = contentWrap.find('span', {'class': 'title_subject'}).get_text(strip = True)
+        words.extend(title.split())
+        writeDivP = contentWrap.find('div', {'class':'write_div'}).find_all('p')
+        for p in writeDivP:
+            article = p.get_text(strip = True)
+            print(article)
+            words.extend(article.split())
+        return contentCrawler(words, gallId, int(dataNum)-1, now, period)        
+    else:
+        return
+
+
+
+# 메인 목록 안 bs에서 최근 게시글의 url과 업로드 시간을 가져오고 현재시간과 비교한 후 최근 게시글의 gallid 랑 게시글인덱스 반환 
+def firstListParsing(bs, now, period):
+    
+    list = bs.find('tr', {'class': 'ub-content us-post', 'data-type':'icon_txt'})
+    if list == None:
+        print('list 파싱 문제!')
+        return -1
+    #만약 삭제된 게시글이라 upTime이 None이 지정되면 다음 게시글로 이동해서 파싱
+    
+    upTime = list.find('td', {'class': 'gall_date'})
+    # if upTime == None:
+    #     print('upTime 파싱 문제!')
+    #     return -1
+    
+    upTimeTitle = upTime.attrs['title']
+    dt = datetime.strptime(upTimeTitle, "%Y-%m-%d %H:%M:%S")
+    diff = now - dt
+    if (diff.days <= period):
+        firstNum = list.attrs['data-no']
+        gallName = bs.find('button',{'id':'headTail_tab_gall'}).find('p',{'class': 'gallname'}).attrs['data-gallid']
+        print('첫 게시글({}) 파싱 성공!'.format(firstNum))
+        return gallName, firstNum
+    else:
+        return -1
+
+#!!!!!반드시 vpn 키고 돌릴것!!!!!
+def main():
+    now = datetime.now()
+    period = 1 #{perid}일 기준 이내 게시글 크롤링
+    # 메인페이지 url로 html 파싱 시작
+    url ='https://gall.dcinside.com/mgallery/board/lists/?id=stockus'
+    req = Request(url, headers= {'User-Agent': 'Mozilla/5.0'})
     html = urlopen(req)
     bs = BeautifulSoup(html, 'html.parser')
-    content = bs.find('script', {'type': 'application/ld+json'})
-    data = json.loads(content.string)
-    title = data.get("headline")
-    articleBody = data.get("articleBody")
-    titleList = title.rsplit("-", 1)[0].strip().split(" ")
-    articleBodyList = articleBodyParser(articleBody)
+    words= []
+    # 크롤링 시점 기준 가장 최근 게시글 파싱 함수 ; 리턴 : gallid, 게시글번호 ; 만약, 설정한 기간내의 게시글이 없을때 -1 반환
+    fLP = firstListParsing(bs, now, period)
+    if fLP==-1:
+        print('크롤링 실패! 해당되는 게시글이 없습니다.')
+        return 0;    
+    contentCrawler(words, fLP[0], fLP[1], now, period)
 
-    for s in titleList:
-        print(s)
-    for s in articleBodyList:
-        print(s)
-
-def articleBodyParser(articleBody):
-    suffix = "- dc official App"
-    if articleBody.endswith(suffix):
-        articleBody = articleBody[:-len(suffix)]
-    articleBodyList = articleBody.strip().split(" ")
-    return articleBodyList
-
-
-def main():
-    contentCrawler('https://gall.dcinside.com/mgallery/board/view/?id=stockus&no=14603145&exception_mode=recommend&page=1')
-
+    for w in words:
+        print(w)
+    return 0;
 
 if __name__ == "__main__":
     main()
