17d3054139d0b045abe10d5c83f4d58acb8cbfc6
mins-oo <msjhkim04@naver.com>
2026-03-16 21:51:03 +0900

    feat: 라이브러리 변경 및 Dockerfile 수정에 따른 requirements.txt 추가, json 파일 수정
    soynlp에서 transformer로 변경
    단어가 아닌 문장 단위로 긍/부정 값을 구분(label, score)
    
    추후 model을 Dockerfile에서 미리 설치하는 것으로 변경 필요

--- src/NLP.py
+++ src/NLP.py
@@ -1,47 +1,15 @@
-from soynlp.word import WordExtractor
-from soynlp.noun import LRNounExtractor_v2
-from soynlp.tokenizer import LTokenizer
-from soynlp.normalizer import repeat_normalize
+from transformers import pipeline
+import pandas as pd
 
-sentences = [
-    "이거 킹받네 ㅋㅋㅋㅋㅋㅋㅋㅋ",
-    "아니 진짜 ㅋㅎㅋㅎㅋㅎㅋㅎㅋㅎ",
-    "이번 주식 장 마감 실화냐? 어이가 없네",
-    "주식 투자 너무 어려워요 ㅠㅠㅠㅠㅠㅠ",
-    "킹받네 라는 말 요즘 너무 많이 쓰는 듯",
-    "실화냐 진짜 대박이다",
-    "오늘 점심 메뉴 실화냐? 너무 맛없어"
-]
+#crawling.py에서 Word={gellID, time, wordContent, sentiment}를 리스트로 전달
+#wordList = Word
 
-#문장 내 반복 표현 정규화
-sentences = [repeat_normalize(s, num_repeats=2) for s in sentences]
+#sentiment 파이프라인 생성, KR-BERT 모델 사용
+classifier = pipeline("sentiment-analysis", model="jaehyeong/koelectra-base-v3-generalized-sentiment-analysis")
 
-# 2. 단어 추출기 학습 (WordExtractor)
-# 응집 확률(cohesion)과 브랜칭 엔트로피(entropy)를 계산하여 단어를 찾습니다.
-word_extractor = WordExtractor()
-word_extractor.train(sentences)
-word_scores = word_extractor.extract()
+#sentences = wordList.wordContent
+sentences = ["트럼프 참 잘하는 짓이다"]
 
-# '킹받네'라는 단어의 점수 확인 예시
-if '킹받네' in word_scores:
-    print(f"--- '킹받네' 분석 결과 ---")
-    print(f"응집도(Cohesion): {word_scores['킹받네'].cohesion_proxy:.4f}")
-    print(f"통계적 독립도(Entropy): {word_scores['킹받네'].right_branching_entropy:.4f}\n")
-
-# 3. 명사 추출기 (NounExtractor) - 커뮤니티 용어 핵심 키워드 뽑기
-noun_extractor = LRNounExtractor_v2(verbose=False)
-nouns = noun_extractor.train_extract(sentences) # 학습과 추출을 동시에
-
-print("--- 추출된 명사 목록 ---")
-for noun, score in nouns.items():
-    # score.frequency는 빈도수입니다.
-    print(f"{noun} (빈도: {score.count})")
-
-# 4. 토크나이징 (LTokenizer)
-# 위에서 학습한 점수를 바탕으로 문장을 단어 단위로 쪼갭니다.
-scores = {word:score.cohesion_proxy for word, score in word_scores.items()}
-tokenizer = LTokenizer(scores=scores)
-
-test_sentence = "오늘 주식 장 마감 실화냐 진짜 킹받네"
-print(f"\n--- 토큰화 결과 ---")
-print(tokenizer.tokenize(test_sentence))
+ret = classifier(sentences)
+for i in range(len(sentences)):
+    print(f"\n\"{sentences[i]}\"\nlabel: {ret[i]['label']}\tscore: {ret[i]['score']:.4f}")
