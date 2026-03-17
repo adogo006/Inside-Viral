d40a463db3f841bc4eed300cf22cb8369430ecc3
adogo006 <peand006@gmail.com>
2026-03-17 08:53:46 +0000

    feat: models 타입에 state 속성 추가
    state 속성은 str 형태로 pending 과 completed로 구분된다 자세한 내용은 코드 참고

--- DB_manager/models.py
+++ DB_manager/models.py
@@ -3,6 +3,11 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, TEXT
 from sqlalchemy.sql import func
 from database import Base
+from enum import Enum
+
+class ProcessState(str, Enum):
+    PENDING = "pending"         # 크롤링 직후
+    COMPLETED = "completed"   # NLP 직후
 
 class Word(Base):
     __tablename__ = 'words'
@@ -11,3 +16,4 @@ class Word(Base):
     date = Column(DateTime(timezone= True))
     wordContent = Column(TEXT)
     sentiment = Column(Float)
+    state = Column(String(20), default = ProcessState.PENDING) # pending 기본값, nlp 처리 후 completed 로 변경
