7adfb16c2cc65d454f91926d254a4de05ff5212f
adogo006 <peand006@gmail.com>
2026-03-25 06:20:17 +0000

    feat: api 관련 라이브러리 각 환경폴더안 requirements 에 추가

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
-from .database import SessionLocal # 기존에 만드신 DB 설정 파일
-from . import models, schemas    # 모델과 데이터 규격
+from DB_manager.database import SessionLocal # 기존에 만드신 DB 설정 파일
+import DB_manager.models
+from crawling.main import task_crawl_and_save
 
 app = FastAPI(title="InsideViral API")
+#insideViral-net 전용 내부포트 URL
+CRAWLER_URL = "http://crawler:8002"
+NLP_URL = "http://nlp:8001"
 
 # DB 세션 의존성 주입 (매 요청마다 DB 연결/해제 관리)
 def get_db():
@@ -19,15 +23,16 @@ def read_root():
     return {"message": "Welcome to InsideViral API Server"}
 
 # 1. 특정 갤러리의 데이터 가져오기 (예: 공포 지수 산출용)
-@app.get("/words/{gall_id}", response_model=List[schemas.Word])
-def get_gallery_data(gall_id: str, db: Session = Depends(get_db)):
-    data = db.query(models.Word).filter(models.Word.gallId == gall_id).all()
-    if not data:
-        raise HTTPException(status_code=404, detail="Gallery data not found")
-    return data
+# @app.get("/words/{gall_id}", response_model=List[schemas.Word])
+# def get_gallery_data(gall_id: str, db: Session = Depends(get_db)):
+#     data = db.query(models.Word).filter(models.Word.gallId == gall_id).all()
+#     if not data:
+#         raise HTTPException(status_code=404, detail="Gallery data not found")
+#     return data
 
 # 2. 크롤링 시작 명령 (POST 요청)
-@app.post("/crawl/{gall_id}")
-def start_crawling(gall_id: str):
-    # 여기서 크롤링 컨테이너에 신호를 보내는 로직이 들어갑니다.
+@app.post("/crawl/{gall_main_url}")
+# /crawl/{gall_main_url}?days=x&days_ago=y
+async def request_crawling(gall_main_url: str, days: int =1, days_ago: int = 0):    
+    response = await client.get
     return {"status": "success", "message": f"Crawling started for {gall_id}"}
