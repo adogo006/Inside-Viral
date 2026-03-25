7cef22b9c069295fbab4a63da50be5c705884ad2
adogo006 <peand006@gmail.com>
2026-03-25 16:03:40 +0900

    refactor: .env 파일 위치 수정 및 필요 라이브러리 정리, 각 컨테이너에서 env 불러올수 있도록 수정
    os.getenv(속성이름)을 통해서 각 값을 가져올 수 있습니다

--- DB_manager/database.py
+++ DB_manager/database.py
@@ -4,9 +4,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
-load_dotenv()
 DB_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 try: 
